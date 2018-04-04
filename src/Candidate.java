f2d70c7658b3977be034d06b90a22812e1a045b9
Amauri de Melo Junior <amaurimelojunior@gmail.com>
2018-04-04 20:42:36 -0300

    Criando classe Candidate (candidato/aluno)

---
+++ src/Candidate.java
@@ -0,0 +1,14 @@
+public class Candidate
+{
+    protected double theta;
+
+    public Candidate(double theta)
+    {
+        this.theta = theta;
+    }
+    
+    public getTheta()
+    {
+        return this.theta;
+    }
+}
