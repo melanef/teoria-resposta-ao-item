791721dc6bbaa024527ec807b0303325c5056e81
Amauri de Melo Junior <amaurimelojunior@gmail.com>
2018-04-04 20:29:05 -0300

    Restaurando PATTERN para usar no matches

--- src/QuestionLibrary.java
+++ src/QuestionLibrary.java
@@ -7,40 +7,41 @@ import java.util.ArrayList;
 
 public class QuestionLibrary
 {
-	public static final String SEPARATOR = " ";
+    public static final String PATTERN = "[0-9\.]+ [0-9\.]+";
+    public static final String SEPARATOR = " ";
 
-	protected ArrayList<Question> library;
+    protected ArrayList<Question> library;
 	
-	protected QuestionLibrary()
-	{
-		this.library = new ArrayList<Question>();
-	}
+    protected QuestionLibrary()
+    {
+        this.library = new ArrayList<Question>();
+    }
 	
-	public QuestionLibrary createFromFile(String filepath)
-	{
-		BufferedReader reader = new BufferedReader(new FileReader(filepath));
-		QuestionLibrary newLibrary = new QuestionLibrary();
-		
-		String line;
-		String [] parts;
-		while (line = reader.readLine()) {
-			if (line.matches(QuestionLibrary.SEPARATOR)) {
-				parts = line.split(QuestionLibrary.PATTERN);
+    public QuestionLibrary createFromFile(String filepath)
+    {
+        BufferedReader reader = new BufferedReader(new FileReader(filepath));
+        QuestionLibrary newLibrary = new QuestionLibrary();
+
+        String line;
+        String [] parts;
+        while (line = reader.readLine()) {
+            if (line.matches(QuestionLibrary.PATTERN)) {
+                parts = line.split(QuestionLibrary.SEPARATOR);
 				
-				Double a = new Double(parts[0]);
-				Double b = new Double(parts[1]);
+                Double a = new Double(parts[0]);
+                Double b = new Double(parts[1]);
 				
-				newLibrary.add(new Question(a.doubleValue(), b.doubleValue()));
-			}
-		}
-		
-		return newLibrary;
+                newLibrary.add(new Question(a.doubleValue(), b.doubleValue()));
+            }
+        }
+
+        return newLibrary;
 	}
 	
-	public Question getQuestion(integer id)
-	{
-		return (Question) this.library.get(id).clone();
-	}
+    public Question getQuestion(integer id)
+    {
+        return (Question) this.library.get(id).clone();
+    }
     
     public Question getQuestion(Integer id)
     {
