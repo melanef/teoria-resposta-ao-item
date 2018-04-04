b6d3c682dfc1004ec54c2cbba1e15a4fd66a6ae3
Amauri de Melo Junior <amaurimelojunior@gmail.com>
2018-04-04 20:33:32 -0300

    Instanciando Exam's (provas) no Main

--- src/TRI.java
+++ src/TRI.java
@@ -12,5 +12,10 @@ public class TRI {
 	public static void main(String [] args)
 	{
 		QuestionLibrary library = QuestionLibrary.createFromFile(QUESTIONS_PATH);
+		Exams [] exams = Exam[4];
+		exam[0] = new Exam(10, library);
+		exam[0] = new Exam(20, library);
+		exam[0] = new Exam(50, library);
+		exam[0] = new Exam(100, library);
 	}
 }
