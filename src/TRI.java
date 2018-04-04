ff37c7b44ddca57638db8bd533ae8f34a2efc6be
Amauri de Melo Junior <amaurimelojunior@gmail.com>
2018-04-04 19:32:16 -0300

    Atualizando TRI incluindo método de entrada main
    Incluindo constante de endereço do arquivo de dados das questões

--- src/TRI.java
+++ src/TRI.java
@@ -1,8 +1,16 @@
 import java.lang.Math;
+import java.lang.String;
 
 public class TRI {
+	public static final String QUESTIONS_PATH = "../data/questoes.txt";
+
 	static double chance(double theta, double ai, double bi)
 	{
 		return Math.pow(Math.E, ai * (theta - bi)) / 1.0 + Math.pow(Math.E, ai * (theta - bi));
+	}
+	
+	public static void main(String [] args)
+	{
+		QuestionLibrary library = QuestionLibrary.createFromFile(QUESTIONS_PATH);
 	}
 }
