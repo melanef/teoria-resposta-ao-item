4725190dd4b741f00148a05b67dab3e295d7adf1
Amauri de Melo Junior <amaurimelojunior@gmail.com>
2018-04-04 20:46:48 -0300

    Instanciando candidatos com diferentes thetas

--- src/TRI.java
+++ src/TRI.java
@@ -2,20 +2,27 @@ import java.lang.Math;
 import java.lang.String;
 
 public class TRI {
-	public static final String QUESTIONS_PATH = "../data/questoes.txt";
+    public static final String QUESTIONS_PATH = "../data/questoes.txt";
 
-	static double chance(double theta, double ai, double bi)
-	{
-		return Math.pow(Math.E, ai * (theta - bi)) / 1.0 + Math.pow(Math.E, ai * (theta - bi));
-	}
+    public static double chance(double theta, double ai, double bi)
+    {
+        return Math.pow(Math.E, ai * (theta - bi)) / 1.0 + Math.pow(Math.E, ai * (theta - bi));
+    }
 	
-	public static void main(String [] args)
-	{
-		QuestionLibrary library = QuestionLibrary.createFromFile(QUESTIONS_PATH);
-		Exams [] exams = Exam[4];
-		exam[0] = new Exam(10, library);
-		exam[0] = new Exam(20, library);
-		exam[0] = new Exam(50, library);
-		exam[0] = new Exam(100, library);
+    public static void main(String [] args)
+    {
+        QuestionLibrary library = QuestionLibrary.createFromFile(QUESTIONS_PATH);
+        Candidate [] candidates = Candidate[5];
+        candidates[0] = new Candidate(-1.0);
+        candidates[1] = new Candidate(-0.5);
+        candidates[2] = new Candidate(0.0);
+        candidates[3] = new Candidate(0.5);
+        candidates[4] = new Candidate(1.0);
+        
+        Exam [] exams = Exam[4];
+        exams[0] = new Exam(10, library);
+        exams[1] = new Exam(20, library);
+        exams[2] = new Exam(50, library);
+        exams[3] = new Exam(100, library);
 	}
 }
