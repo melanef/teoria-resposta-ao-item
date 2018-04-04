72ef0bebce25f18dab5d45d797b2fd2e66d55c91
Amauri de Melo Junior <amaurimelojunior@gmail.com>
2018-04-04 20:50:18 -0300

    Identação(?)

--- src/Exam.java
+++ src/Exam.java
@@ -2,23 +2,23 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Exam
 {
-	protected ArrayList<Question> questions;
-	protected ArrayList<Integer> question_ids;
-	protected int questions_quantity;
+    protected ArrayList<Question> questions;
+    protected ArrayList<Integer> question_ids;
+    protected int questions_quantity;
 	
-	public Exam(int questions_quantity)
-	{
-		this.questions_quantity = questions_quantity;
-		this.questions = new ArrayList<Question>(this.questions_quantity);
-	}
+    public Exam(int questions_quantity)
+    {
+        this.questions_quantity = questions_quantity;
+        this.questions = new ArrayList<Question>(this.questions_quantity);
+    }
     
     public Exam(int questions_quantity, QuestionLibrary library) {
         this(questions_quantity);
         this.fillQuestions(library);
     }
 	
-	public void fillQuestions(QuestionLibrary library)
-	{
+    public void fillQuestions(QuestionLibrary library)
+    {
         if (this.library.size() == this.questions_quantity) {
             for (int i = 0; i < this.questions_quantity; i++) {
                 this.questions.add(library.getQuestion(new Integer(i));
@@ -26,15 +26,15 @@ public class Exam
             }
         }
         
-		while (this.questions.size() < this.questions_quantity)
-			Integer current_question_id = new Integer(
-				ThreadLocalRandom.current().nextInt(0, library.size() + 1)
-			);
+        while (this.questions.size() < this.questions_quantity)
+            Integer current_question_id = new Integer(
+                ThreadLocalRandom.current().nextInt(0, library.size() + 1)
+            );
 
-			if (!this.question_ids.contains(current_question_id)) {
-				this.question_ids.add(current_question_id);
-				this.questions.add(library.getQuestion(current_question_id);
-			}
-		}
-	}
+            if (!this.question_ids.contains(current_question_id)) {
+                this.question_ids.add(current_question_id);
+                this.questions.add(library.getQuestion(current_question_id);
+            }
+        }
+    }
 }
