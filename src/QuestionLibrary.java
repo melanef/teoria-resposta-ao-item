d499279b0a628cd8e1aaf653a4ca7855519670a0
Amauri de Melo Junior <amaurimelojunior@gmail.com>
2018-04-03 22:35:06 -0300

    Criando classe QueryLibrary
    A QueryLibrary é um repositório de questões construído a partir da leitura de um arquivo (e por isso seu aspecto de facade).

---
+++ src/QuestionLibrary.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.Double;
+import java.lang.String;
+import java.util.ArrayList;
+
+public class QuestionLibrary
+{
+	public static final PATTERN = "[0-9\.]+ [0-9\.]+";
+
+	protected ArrayList<Question> library;
+	
+	protected QuestionLibrary()
+	{
+		this.library = new ArrayList<Question>();
+	}
+	
+	public QuestionLibrary createFromFile(String filepath)
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(filepath));
+		QuestionLibrary newLibrary = new QuestionLibrary();
+		
+		String line;
+		String [] parts;
+		while (line = reader.readLine()) {
+			if (line.matches(QuestionLibrary.PATTERN)) {
+				parts = line.split(QuestionLibrary.PATTERN);
+				
+				Double a = new Double(parts[0]);
+				Double b = new Double(parts[1]);
+				
+				newLibrary.add(new Question(a.doubleValue(), b.doubleValue()));
+			}
+		}
+		
+		return newLibrary;
+	}
+}
