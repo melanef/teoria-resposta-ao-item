b949a33726d1ccac403f71638ef42046528c436f
Amauri de Melo Junior <amaurimelojunior@gmail.com>
2018-04-03 22:13:09 -0300

    Criando classe para Questão
    Contém os dois parâmetros de discriminação (a) e de dificuldade (b) da questão.

---
+++ src/Question.java
@@ -0,0 +1,21 @@
+public class Question
+{
+	protected double a;
+	protected double b;
+	
+	public Question(double a, double b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+	
+	public double get_a()
+	{
+		return this.a;
+	}
+	
+	public double get_b()
+	{
+		return this.b;
+	}
+}
